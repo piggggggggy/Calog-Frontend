dd3af391e84264bfcc19047ce233352787031043
choijeehyuk <jeehyuk97@daum.net>
2021-07-28 22:34:53 +0900

    ✅ 버튼, 그리드 height 100% 제거

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Kakao from '../components/social/Kakao';
 import { Input } from '../elements';
+import styled from 'styled-components';
 /** 
  * @param {*} props
  * @returns 설명적기
@@ -17,76 +18,16 @@ const Login = (props) => {
 
   return (
     <React.Fragment>
+      <Container>
         <h1>로그인 페이지</h1>
-        <p>주요 콘텐츠로 이동접근성 도움말
-접근성 관련 의견 보내기
-Google
-1600 5139
-
-전체
-지도이미지쇼핑동영상더보기
-도구
-검색결과 약 20,800,000개 (0.50초) 
-도움말: 한국어 검색결과만 검색합니다. 환경설정에서 검색 언어를 지정할 수 있습니다.
-
-0216005139 / 02-1600-5139 | 더콜 스팸 전화번호부http://www.thecall.co.kr › bbs › board
-2020. 10. 27. — 더콜에서 329회 이상 조회된 전화번호 0216005139 / 02-1600-5139 에 대한 자세한 정보를 확인하세요.
-1600 5139 관련 이미지
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-1600 5139에 대한 이미지 검색결과
-모두 보기
-피드백
-모두 보기
-
-JUNKCALL.org 대한민국 0216005139, 02-1600-5139 검색 결과https://junkcall.org › q=02-1600-5139
-JUNKCALL.org 대한민국 0216005139, 02-1600-5139 검색 결과. Search in https://junkcall.org/us/?q=0216005139. install Whoscall. 결과 없음. 정보를 남겨주세요.
-
-16005139 / 1600-5139 - 이 번호의 정체는? - 후스넘버http://whosnumber.com › 16005139
-이 번호는 누구지? 어디에서 걸려온 전화야? 16005139 / 1600-5139 이런 모든 의문의 전광석화와 같은 해답!
-
-OBH Nordica ARTIST ALIZE 1600 / 5139 - Elonhttps://www.elon.se › obh-nordica-artist-alize-1600-5139
-Köp OBH Nordica ARTIST ALIZE 1600 / 5139 hos Elon. ARTIST ALIZE 1600 / 5139 har följande fördelar: 1600 watt|2 hastigheter och värmelägen|Coolfunktion.
-
-OBH Nordica Artist Alize 5139 • Se lägsta pris (1 butiker)https://www.pricerunner.se › Skönhet › Hårtorkar
-OBH Nordica Artist Alize 5139. 1600W, Kalluftsfunktion, Upphängningsöggla, Fönmunstycke, 2 Värmelägen, 2 Hastighetslägen, Sladdlängd: 2.8m. Pris 399 kr.
-
-【アスクル】立川機工 FIRSTAGE ファーステージ ロール ...https://www.askul.co.jp › ...
-立川機工 FIRSTAGE ファーステージ ロールスクリーン 洗える 1600×400mm ショコラ TR-5139 1台（直送品）の通販ならアスクル。最短当日または翌日以降お届け。
-
-Tables for the Energy and Photon Distribution in Equilibrium ...https://books.google.co.kr › books
-P. A. Apanasevich, ‎V. S. Aizenshtadt · 2013 · ‎Science
-... 9959 – 6 || 1 |05 – 5 | 1219 – 5 | 1600 5139 – 6 || 5969 – 6 6860 – 6 || 7811 – 6 8818 – 6 || 9880 – 6 1099 – 5 | 1216 – 5 | 1650 4997 – 6 5828 – 6 6724 ...
-
-Summary of Foreign Commerce of the United Stateshttps://books.google.co.kr › books
-United States. Bureau of the Census · 1944 · ‎Commercial statistics
-2,529 823,874 493 1,600 5,139 5,382 27,107 423 2,085 4,216 Jute , unmanufactured ( 3241.000 ) ........... .ton .. Jute butts ( 3242.000 ) . ...... ton .
-
-Geological Survey Water-supply Paperhttps://books.google.co.kr › books
-1964 · ‎Irrigation
-... 3,713 3,610 2,984 2,578 2,452 5,139 5,802 4,717 3,848 3,839 1956 1957 1959 1959 ... 1954 14,300 June 17 , 1955 1,700 2,300 2,030 1,780 1,600 5,139 5,802 ...
-1	
-2
-3
-4
-5
-6
-7
-8
-9
-10
-다음
-대한민국
-427-010 경기도 과천시 중앙동 - IP 주소 기반 - 위치 업데이트
-도움말의견 보내기개인정보처리방침약관</p>
+        <Input width="100px" padding="10px" border_radius="26px"/>
+        <InputB/>
         <Kakao/>
+        <Kakao/>
+        <Kakao/>
+        <Kakao/>
+        <Kakao/>
+        </Container>
     </React.Fragment>
   );
 }
@@ -94,3 +35,16 @@ Geological Survey Water-supply Paperhttps://books.google.co.kr › books
 
 export default Login;
 
+const Container = styled.div`
+  background-color: #eee;
+  height: 896px;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const InputB = styled.input`
+  width: 100px;
+`;
