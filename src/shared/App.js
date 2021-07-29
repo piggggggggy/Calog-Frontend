b9ec50abee41a397ea9802167e22806b4e2f6fad
skdud4659 <skdud4659@gmail.com>
2021-07-29 09:58:51 +0900

    ✨헤더, 대시보드 추가
    - 헤더 (로고헤더, 버튼헤더)
    - 대시보드 생성!

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import GlobalStyles from './GlobalStyles';
 //라우팅
 import { Route } from "react-router-dom";
+import {DashBoard} from '../pages'
 //로그인 임시
 import Example from '../pages/Example';
 //캘린더 임시
@@ -13,6 +14,7 @@ const App = (props) => {
     <React.Fragment>
       <GlobalStyles/>
       <Wrap>
+      <Route path="/dashboard" exact component={DashBoard}/>
       {/* 로그인 임시 */}
       <Route path="/login" exact component={Example}/>
       {/* calendar 테스트 라우팅 나중에 지울게요! */}
