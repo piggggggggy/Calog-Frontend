e58229b31938b390a928ca32da5fda3c45d341c6
skdud4659 <skdud4659@gmail.com>
2021-07-30 03:13:58 +0900

    💄app.js에 ThemeProvider

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {ThemeProvider} from 'styled-components';
 import GlobalStyles from './GlobalStyles';
 //라우팅
 import { Route } from "react-router-dom";
 import {DashBoard, Login, Signup, Main, Calendar} from '../pages'
 import Nav from './Nav';
-
+//테마
+import theme from './theme';
 
 const App = (props) => {
   return (
     <React.Fragment>
+<<<<<<< Updated upstream
       <GlobalStyles />
       <Wrap>
 <<<<<<< Updated upstream
@@ -32,6 +34,21 @@ const App = (props) => {
         <Route path="/calendar" exact component={Calendar}/>
         <Nav />
       </Wrap>
+=======
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Wrap>
+          <Route path="/" exact component={Main} />
+          <Route path="/fooddetail" exact component={FoodDetail} />
+          <Route path="/cart" exact component={Cart} />
+          <Route path="/dashboard" exact component={DashBoard}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/signup" exact component={Signup}/>
+          <Route path="/calendar" exact component={Calendar}/>
+          <Nav />
+        </Wrap>
+      </ThemeProvider>
+>>>>>>> Stashed changes
     </React.Fragment>
   );
 }
