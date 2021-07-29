3e26e6a10a6d1aca872d66afeceb46868a1ec4cf
choijeehyuk <jeehyuk97@daum.net>
2021-07-29 10:11:51 +0900

    ✅ 머지 전 커밋

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,16 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 const initialState = {
-  user_info: "",
+  user_info: {email: "email", nickname: "nickname"},
   is_login: false,
 };
 
-export const SetUserCheck = () => {
+export const SetUserCheck = (user_info) => {
     console.log("click setUserCheck")
-    const user_info = {username:"jeehyuk"}
-    return function(dispatch, getState){
+    //  {email:"jeehyuk97@daum.net", nickname:"sungsu"}
+    return function(dispatch, getState, {history}){
         dispatch(SetUser(user_info))
     }
 }
@@ -21,11 +19,13 @@ const user = createSlice({
   initialState,
   reducers: {
     SetUser: (state, action) => {
-    //   state.user_info = action.payload.username;
+      state.user_info = action.payload;
       state.is_login = true;
-      state.user_info = action.payload.username;
     },
-
+    LogOut: (state, action) => {
+        state.user_info = action.payload;
+        state.is_login = false;
+    }
   },
 });
 
