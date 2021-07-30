70aa3fad316216321f00b05ae65e12ce7d85e14d
YONG-TAE PARK <pyt4105@gmail.com>
2021-07-30 19:44:49 +0900

    :lipstick: Main, Cart  디자인 적용 뷰 1차 완성

--- src/components/Cart_Card.js
+++ src/components/Cart_Card.js
@@ -37,12 +37,12 @@ const Card = (props) => {
         > */}
         {/* <div style={{height: 44}}> swipeout demo </div> */}
         <FoodCard>
-          <Text margin="0" padding="0">쌀 밥</Text>
-          <Text size="1.8rem" bold color="green" margin="10px 0 0 0" padding="0">310 kcal</Text>
+          <Text lineheight="22px" size="17px" margin="0" padding="0">쌀밥</Text>
+          <Text lineheight="28px" size="22px" bold color="#2A2A2A" margin="8px 0 0 0" padding="0">310 kcal</Text>
           <CountBox>
-            <div><IoIosArrowDropleft color="gray" size="2rem"/></div>
-            <Text color="#353535" bold size="2rem" margin="0" padding="0">5</Text>
-            <div><IoIosArrowDropright color="gray" size="2rem"/></div>
+            <div><IoIosArrowDropleft color="gray" size="27px"/></div>
+            <Text lineheight="28px" color="#000000" bold size="22px" margin="0" padding="0">5</Text>
+            <div><IoIosArrowDropright color="gray" size="27px"/></div>
           </CountBox>
         </FoodCard>
       {/* </Swipeout> */}
@@ -59,22 +59,35 @@ const FoodCard = styled.div`
   position: relative;
   /* display: flex; */
   width: calc(100% - 50px);
-  margin: 0.5rem 25px;
-  padding: 25px 30px;
-  background: lightgray;
-  border: none;
-  border-radius: 20px;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-bottom: 8px;
+  padding: 25px 26px 23px 26px;
+  background: #fff;
+  border: 1px solid #F19F13;
+  border-radius: 28px;
 `;
 
 const CountBox = styled.div`
   position: absolute;
   right: 5%;
-  top: 40%;
+  top: 39px;
   display: flex;
   align-items: center;
   justify-content: space-between;  
   position: absolute;
-  width: 115px;
+  gap: 26px;
+
+  & > div {
+    width: 27px;
+    height: 27px;
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+    /* display: flex;
+    align-items: center;
+    justify-content: center; */
+  }
 `;
 
 export default Card;
--- src/components/Main_MainBody.js
+++ src/components/Main_MainBody.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 // elements & components
-import { Grid } from '../elements';
+import { Grid, Text } from '../elements';
 import Card from './Main_Card';
 import RangeSlider from './RangeSlider';
 // icon
 import { BiSearchAlt2 } from 'react-icons/bi';
+import { IoIosArrowDown } from 'react-icons/io';
 
 /** 
  * @param {*} props
@@ -24,29 +25,30 @@ const MainBody = (props) => {
     <React.Fragment>
       <BodyContainer>
 
-        <SearchGrid margin="1rem 0.5rem">
+        <SearchGrid>
           <div>
-            <input placeholder="입력해"/>
+            <input placeholder="어떤 칼로리가 궁금하신가요?"/>
             <div>
-              <BiSearchAlt2 size="1rem" />
+              <BiSearchAlt2 size="24px" color="#5F5F5F" />
             </div>
           </div>
         </SearchGrid>
 
-        {/* <Grid display="flex" jc="center" padding="0 20px">
-          {category_list.map((c, idx) => {
-            return (
-              <CategoryButton key={idx}>{c}</CategoryButton>
-            )
-          })}
-        </Grid> */}
-
-        <Grid margin="30px 0">
+        <Grid margin="auto" width="90%" >
           <RangeSlider 
             min={0}
             max={5000}
             onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
           />
+        </Grid>
+
+        <Grid margin="42px 0 25px 0" padding="0 25px" display="flex" jc="flex-end">
+          <Grid display="flex" jc="flex-end" width="35%" cursor>
+            <Text size="13px" color="#8C8C8C" lineheight="18px" margin="0 5px 0 0" padding="0">칼로리높은순</Text>
+            <Grid width="16px" height="16px" display="flex" jc="center">
+              <IoIosArrowDown size="14px" color="8C8C8C"/>
+            </Grid>
+          </Grid>
         </Grid>
 
         <CardContainer>
@@ -71,49 +73,54 @@ MainBody.defaultProps = {
 
 const BodyContainer = styled.div`
   max-width: 100%;
-  padding: 0 25px;
 `;
 
 const SearchGrid = styled.div`
-  margin: 10px 0;
+  margin: 18px 25px 11px 25px;
   
   & > div {
-    display: flex;
+    position: relative;
+    /* display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-between; */
     width: 100%;
-    border: 1px solid #000;
-    padding: 9px 20px;
-    border-radius: 25px;
+    border: 1px solid #F19F13;
+    padding: 12px 25px;
+    border-radius: 31px;
 
     & > input {
       display: block;
-      font-size: 14px;
+      padding: 0;
+      font-size: 13px;
       font-weight: 400;
       width: 100%;
       border: none;
       outline: none;
-      line-height: 1rem;
+      line-height: 18px;
     }
 
     & > div {
+      position: absolute;
+      padding: 4px;
+      margin: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
       cursor: pointer;
-      padding-left: 10px;
+      right: 13px;
+      top: 5px;
     }
   }
 `;
 
-
-
-
-
 const CardContainer = styled.div`
   margin-top: 60px;
-  width: 100%;
-  height: 100%auto;
+  max-width: 370px;
+  margin: auto;
+  height: 100%;
   display: flex;
   flex-wrap: wrap;
-  column-gap: 6%;
+  column-gap: 4%;
 `;
 
 
