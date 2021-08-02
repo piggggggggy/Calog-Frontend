c19f4ee70530f3114e86d428114b3849841ceddb
YONG-TAE PARK <pyt4105@gmail.com>
2021-08-02 17:22:19 +0900

    :sparkles: favorite 기능 1차 구현

---
+++ src/redux/modules/favorite.js
@@ -0,0 +1,86 @@
+// @역할 : 검색결과 출력, 검색결과 필터링, 장바구니 담기
+// @담당자 : 박용태
+
+import { createSlice } from "@reduxjs/toolkit";
+//전역 > 서버 배포
+import instance from "./instance";
+
+// middleware 
+// 즐겨찾기
+export const addFavoriteDB = (data) => {
+  return function (dispatch, getState, {history}) {
+    instance
+      .post('/api/home/favorite/add', {foodId: data.foodId})
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err, "에러가 났읍니다.")
+      }) 
+  }
+};
+
+// 즐겨찾기 해제
+export const deleteFavoriteDB = (foodId) => {
+  return function (dispatch, getState, {history}) {
+    instance
+      .delete('/api/home/favorite/delete', {foodId: foodId})
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err, "에러가 났읍니다.")
+      }) 
+  }
+};
+
+// 즐겨찾기 목록 가져오기
+export const getFavoriteDB = () => {
+  return function (dispatch, getState, {history}) {
+    instance
+      .get('/api/home/favorite/list')
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err, "에러가 났읍니다.")
+      }) 
+  }
+};
+
+
+// initial State 
+const initialState = {
+  //검색 결과 리스트
+  list : [],
+}
+
+// redux
+const favorite = createSlice({
+  name: "favorite",
+  initialState,
+  reducers: {
+    // 즐겨찾기 추가
+    addFavorite : (state, action) => {
+      state.list.unshift(action.payload);
+    },
+    // 즐겨찾기 삭제
+    deleteFavorite : (state, action) => {
+      const deleted_list = state.list.filter((favo, idx) => {
+        if (favo.foodId !== action.payload) {
+          return favo
+        };
+      });
+      state.list = deleted_list;
+    },
+    // 즐겨찾기 목록 가져오기
+    getFavorite : (state, action) => {
+      state.list = action.payload;
+    },
+
+  }
+});
+
+export const {addFavorite, deleteFavorite, getFavorite} = favorite.actions;
+
+export default favorite;
