b4116201bdee1a7ba049f95c8d4af4a38eac626d
skdud4659 <skdud4659@gmail.com>
2021-08-02 21:06:27 +0900

    💄 record height 수정 / header history 연결

--- src/shared/LogoHeader.js
+++ src/shared/LogoHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Grid} from '../elements';
+//history
+import {history} from '../redux/configStore';
 
 const LogoHeader = (props) => {
   return (
     <React.Fragment>
-      <Grid bg={'gray'} height="44px" width="10%" margin="2% 5%">
+      <Grid bg={'gray'} height="44px" width="10%" margin="2% 5%" _onClick={() => history.push('/')}>
         
       </Grid>
     </React.Fragment>
