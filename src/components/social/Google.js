b44ef0f2ff821d2c8cdeea4a7742b453e775c094
choijeehyuk <jeehyuk97@daum.net>
2021-08-09 17:34:29 +0900

    ✅ 소셜로그인 연결 중

--- src/components/social/Google.js
+++ src/components/social/Google.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
 
-const clientId = "984018886520-t7djptr1njd2q1pjen3gipgmpq4ar2ek.apps.googleusercontent.com";
+const Google = () => {
 
-export default function GoogleButton({ onSocial }){
-    const onSuccess = async(response) => {
-    	console.log(response);
-    
-        const { googleId, profileObj : { email, name } } = response;
-    }
 
-    const onFailure = (error) => {
-        console.log(error);
-    }
 
-    return(
-        <div>
-            <GoogleLogin
-                clientId={clientId}
-                responseType={"id_token"}
-                onSuccess={onSuccess}
-                onFailure={onFailure}/>
-        </div>
-    )
+  return (
+    <div>
+        구글
+    </div>
+  );
 }
+
+
+export default Google;
--- src/components/social/Kakao.js
+++ src/components/social/Kakao.js
@@ -1,42 +1,15 @@
 import React from 'react';
-import {Button} from '../../elements/index';
-/** 
- * @param {*} props
- * @returns 설명적기
- * @역할 ~~~하는 컴포넌트
- * @필수값 이 컴포넌트를 사용할 때 필수 props
- * @담당자 : 
-*/
 
-const KakaoLogin = () => {
-// dispatch
-// props
-// useEffect
+const Kakao = () => {
 
-function SocialKakao () {
-window.Kakao.init("260d1c32075b38f17bb12e77e11aa4ca");
-    window.Kakao.Auth.login({
-        success: function(authObj) {
-            console.log(authObj);
-            console.log(window.API);
-            window.Kakao.API.request({
-                url:'/v2/user/me',
-                success: res => {
-                    console.log(res);
-                    const profile = res.kakao_account.profile;
-                    console.log(profile);
-                }
-            })
-        }
-    })
-}
+
 
   return (
-    <React.Fragment>
-        <Button _onClick={SocialKakao} bg="#e1bee7" width="8rem" border_radius="20px">카카오 로그인</Button>
-    </React.Fragment>
+    <div>
+        구글
+    </div>
   );
 }
 
 
-export default KakaoLogin;
+export default Kakao;
