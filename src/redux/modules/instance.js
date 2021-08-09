3fe93d4605c88821f7e9f21ef2e6a617904d8bb0
skdud4659 <skdud4659@gmail.com>
2021-08-09 19:09:19 +0900

    ✨ pwa 적용

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const accessToken = document.cookie.split("=")[1];
 
 const instance = axios.create({
-  baseURL : "http://54.180.133.171/",
-  // baseURL: "https://2k1.shop/",
+  // baseURL : "http://54.180.133.171/",
+  baseURL: "https://2k1.shop/",
   // baseURL: "http://52.78.155.48",
   headers: { authorization: `Bearer ${accessToken}`,
 },
