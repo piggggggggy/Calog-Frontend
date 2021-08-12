ec6978cabd2c0cb316c372c1deee230aa50ed890
choijeehyuk <jeehyuk97@daum.net>
2021-08-13 04:02:31 +0900

    🐛 axios interceptors 사용

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
-const accessToken = document.cookie.split("=")[1];
+const getToken = () => {
+  const token = `${document.cookie}`;
+  return token.split("=")[1];
+};
+
 const instance = axios.create({
   baseURL : "http://54.180.133.171/",
   // baseURL: "https://2k1.shop/",
-  // baseURL: "http://52.78.155.48",
-  headers: { authorization: `Bearer ${accessToken}`,
-},
 });
+
+instance.interceptors.request.use(config => {
+  config.headers = { authorization: `Bearer ${getToken()}` };
+  return config;
+});
+
 
 export default instance;
