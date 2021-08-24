c144d557550cd34c9617710ec30a29a2c428d152
choijeehyuk <jeehyuk97@daum.net>
2021-08-24 18:51:21 +0900

    🐛 history listen 삭제

--- src/shared/api.js
+++ src/shared/api.js
@@ -5,6 +5,7 @@ export const get_csrf_token = () => {
     axios({
         method: "get",
         url: "http://52.79.110.219/api/user/csrf-token",
+        // url: "https://2k1.shop/api/user/csrf-token",
     })
     .then((res) => {
         document.cookie = `CSRF_TOKEN=${res.data.csrfToken};`;
