0da1b42e8c5e738b7ea495158785d8b6c384fb5f
choijeehyuk <jeehyuk97@daum.net>
2021-08-26 15:29:02 +0900

    🔖 V1.0.1

--- src/pages/BodySpec.js
+++ src/pages/BodySpec.js
@@ -110,7 +110,7 @@ const BodySpec = (props) =>
                 현재 버전
               </Text>
               <Text size="17px" margin="17px 20px 15px 0px" color="#F19F13">
-                V1.0.0
+                V1.0.1
               </Text>
             </Version>
             <hr color="#F5F5F5"/>
@@ -202,7 +202,7 @@ return (
               현재 버전
             </Text>
             <Text size="17px" margin="17px 20px 15px 0px" color="#F19F13">
-              V1.0.0
+              V1.0.1
             </Text>
           </Version>
           <hr color="#F5F5F5"/>
