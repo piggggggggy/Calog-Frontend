680591c411ecc89f8e828cd401a3076596a1b4fa
YONG-TAE PARK <pyt4105@gmail.com>
2021-08-27 22:57:16 +0900

    :bug: cursor 업데이트

--- src/components/Cart_CartBody.js
+++ src/components/Cart_CartBody.js
@@ -124,41 +124,41 @@ const CartBody = (props) => {
           </Grid>
 
           {/* 푸드 시간 타입 */}
-          <Grid display="flex" margin="2.7vh 0 0 0" m_margin="2.7vh 0 0 0" cursor>
+          <Grid display="flex" margin="2.7vh 0 0 0" m_margin="2.7vh 0 0 0">
             <div onClick={()=>{selectType("morning")}} style={{marginRight: "3%" }}>
-              <Text lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "아침" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">아침</Text>
+              <Text cursor="pointer" lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "아침" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">아침</Text>
               <Dot>
                 <Grid display={type === "아침" ? 'block' : 'none'}>
                   <FaCircle color="#F19F13" size="7px"/>
                 </Grid>
               </Dot>
             </div>
-            <div onClick={()=>{selectType("lunch")}} style={{marginRight: "3%" }} cursor>
-              <Text lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "점심" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">점심</Text>
+            <div onClick={()=>{selectType("lunch")}} style={{marginRight: "3%" }}>
+              <Text cursor="pointer" lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "점심" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">점심</Text>
               <Dot>
                 <Grid display={type === "점심" ? 'block' : 'none'}>
                   <FaCircle color="#F19F13" size="7px"/>
                 </Grid>
               </Dot>
             </div>
-            <div onClick={()=>{selectType("dinner")}} style={{marginRight: "3%" }} cursor>
-              <Text lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "저녁" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">저녁</Text>
+            <div onClick={()=>{selectType("dinner")}} style={{marginRight: "3%" }}>
+              <Text cursor="pointer" lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "저녁" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">저녁</Text>
               <Dot>
                 <Grid display={type === "저녁" ? 'block' : 'none'}>
                   <FaCircle color="#F19F13" size="7px"/>
                 </Grid>
               </Dot>
             </div>
-            <div onClick={()=>{selectType("snack")}} style={{marginRight: "3%" }} cursor>
-              <Text lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "간식" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">간식</Text>
+            <div onClick={()=>{selectType("snack")}} style={{marginRight: "3%" }}>
+              <Text cursor="pointer" lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "간식" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">간식</Text>
               <Dot>
                 <Grid display={type === "간식" ? 'block' : 'none'}>
                   <FaCircle color="#F19F13" size="7px"/>
                 </Grid>
               </Dot>
             </div>
-            <div onClick={()=>{selectType("midnightSnack")}} style={{marginRight: "3%" }} cursor>
-              <Text lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "야식" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">야식</Text>
+            <div onClick={()=>{selectType("midnightSnack")}} style={{marginRight: "3%" }}>
+              <Text cursor="pointer" lineheight="22px" m_lineheight="20px" size="17px" m_size="15px" bold color={type === "야식" ? "black":"#C4C4C4"} padding="0" margin="0 10px 0 0">야식</Text>
               <Dot>
                 <Grid display={type === "야식" ? 'block' : 'none'}>
                   <FaCircle color="#F19F13" size="7px"/>
