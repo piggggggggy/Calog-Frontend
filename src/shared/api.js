0f6506bba2702564bd642bf79c438f76e3c5cd8e
nayeongKim <nayeongFE@gmail.com>
2021-08-31 00:39:05 +0900

    🐛 직접 등록 오류 수정

--- src/shared/api.js
+++ src/shared/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const get_csrf_token = () => {
   axios({
     method: "get",
-    url: "http://52.79.110.219/api/user/csrf-token",
-    // url: "https://2k1.shop/api/user/csrf-token",
+    // url: "http://52.79.110.219/api/user/csrf-token",
+    url: "https://2k1.shop/api/user/csrf-token",
   })
   .then((res) => {
     document.cookie = `CSRF_TOKEN=${res.data.csrfToken};`;
